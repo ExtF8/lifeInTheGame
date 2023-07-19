1a571b17874efb228248d92b0eba1142a09ce362
extf8 <extfield8@gmail.com>
2023-07-19 20:39:37 +0200

    Added commented steps for logic

---
+++ script.js
@@ -0,0 +1,13 @@
+// Get a reference to the canvas element and its drawing context
+
+// Initialize a 2D array representing the game board
+// Each cell can be 0(dead) or 1(alive)
+
+// Add click event listener to the canvas
+
+// Add event listeners to buttons
+
+// Drawing grid
+// Draw a filled rectangle at the cells position
+
+// Update game based on rules of the game
